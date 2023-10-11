9263127fef8781df527d8b91de7e70db42b44c20
qqJustGoOn <66961636+qqJustGoOn@users.noreply.github.com>
2023-10-11 21:59:12 +0300

    added view/hide cart and loaded items from backend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+    // {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: "img/sneakers/1.jpg"},
+    // {title: 'Мужские Кроссовки Nike Air Max 270', price: 15699, imageUrl: "img/sneakers/2.jpg"},
+    // {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12679, imageUrl: "img/sneakers/3.jpg"},
+    // {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: "img/sneakers/4.jpg"},
+    // {title: 'Мужские Кроссовки Under Armour Curry 8', price: 8999, imageUrl: "img/sneakers/5.jpg"},
+    // {title: 'Мужские Кроссовки Nike Kyrie 7', price: 11299, imageUrl: "img/sneakers/6.jpg"},
+    // {title: 'Мужские Кроссовки Jordan Air Jordan 11', price: 10799, imageUrl: "img/sneakers/7.jpg"},
+];
+
 function App() {
+    const [items, setItems] = React.useState([]);
+    const [cartItems, setCartItems] = React.useState([]);
+
+    const [cartOpened, setCartOpened] = React.useState(false);
+
+    React.useEffect(() => {
+        fetch('https://64ccbaab2eafdcdc851a33d0.mockapi.io/items')
+            .then(res => {
+                return res.json();
+            }).then(json => {
+            setItems(json);
+        });
+    }, []);
+
+    const onAddToCart = (obj) => {
+        setCartItems(prev => [...prev, obj])
+    }
+
     return (
         <div className="wrapper clear">
-            <Drawer />
-            <Header />
+            {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)}/>}
+            <Header onClickCart={() => setCartOpened(true)}/>
             <div className="content p-40">
                 <div className="d-flex align-center justify-between mb-40">
                     <h1>Все кроссовки</h1>
@@ -16,10 +45,18 @@ function App() {
                     </div>
                 </div>
 
-                <div className="d-flex">
-                    <Card />
-                    <Card />
-                    <Card />
+                <div className="d-flex flex-wrap ">
+
+
+                    {items.map(item =>
+                        <Card
+                            title={item.title}
+                            imageUrl={item.imageUrl}
+                            price={item.price}
+                            onFavorite={() => console.log('Добавили взакладки')}
+                            onPlus={(obj) => onAddToCart(obj)}
+                        />
+                    )}
                 </div>
             </div>
         </div>
